af3a87176fbe25c1983b30b3457f6678c4db08dd
Slawek <Slawek@Suaweg>
2017-11-20 16:18:42 +0100

    Little improvements at login view. Register form is done (also added register bean).

---
+++ Photobook/src/java/beans/register.java
@@ -0,0 +1,66 @@
+package beans;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+@Named(value = "register")
+@Dependent
+public class register {
+    private String userName;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private String confirmation;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmation() {
+        return confirmation;
+    }
+
+    public void setConfirmation(String confirmation) {
+        this.confirmation = confirmation;
+    }
+    public register() {
+    }
+    
+}
